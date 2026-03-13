ab2715bba3863d59acfce8f415ab870e6c00089e
biniyam-rgb <binigebrie853@gmail.com>
2026-03-12 21:13:56 -0700

    Banner

--- netflix-clone/src/Page/Home/Home.jsx
+++ netflix-clone/src/Page/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../../Comonents/Header/Header'
 import Footer from '../../Comonents/Footer/Footer'
+import Banner from '../../Comonents/Banner/Banner'
 export default function Home() {
   return (
     <>
      <Header/>
+     <Banner/>
      <Footer/>
     </>
   )
