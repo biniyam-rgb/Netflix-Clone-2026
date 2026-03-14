b5d1e33475599aa8e327c9dbd006b3364bd4abdb
biniyam-rgb <binigebrie853@gmail.com>
2026-03-14 04:00:21 -0700

    Row

--- netflix-clone/src/Page/Home/Home.jsx
+++ netflix-clone/src/Page/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from '../../Comonents/Header/Header'
 import Footer from '../../Comonents/Footer/Footer'
 import Banner from '../../Comonents/Banner/Banner'
+import RowList from '../../Comonents/Rows/RowList/RowList'
 export default function Home() {
   return (
     <>
      <Header/>
      <Banner/>
-     <Footer/>
+     <RowList/>    
+      <Footer/>
     </>
   )
 }
